aa47ae3211b1f6591d2716ea52ec935a849393c9
spellmanlab <heskett@ohsu.edu>
2019-03-08 17:29:20 -0800

    fuirther

--- scripts/call.align.sh
+++ scripts/call.align.sh
@@ -3,3 +3,5 @@ sbatch slurm_align_paired.sh ../fastq/e.23.s32.R1.fastq.gz ../fastq/f.23.s32.R2.
 sbatch slurm_align_paired.sh ../fastq/g.22.s31.R1.fastq.gz ../fastq/h.22.s31.R2.fastq.gz neb22
 sbatch slurm_align_paired.sh ../fastq/i.12.s30.R1.fastq.gz ../fastq/j.12.s30.R2.fastq.gz neb12
 sbatch slurm_align_paired.sh ../fastq/c.25.s33.R1.fastq.gz ../fastq/d.25.s33.R2.fastq.gz neb25
+sbatch slurm_align_paired.sh ../fastq/wgEncodeCshlLongRnaSeqGm12878NucleusLongnonpolyaFastqRd1Rep1.fastq.gz ../fastq/wgEncodeCshlLongRnaSeqGm12878NucleusLongnonpolyaFastqRd2Rep1.fastq.gz gm12878.rep1
+sbatch slurm_align_paired.sh ../fastq/wgEncodeCshlLongRnaSeqGm12878NucleusLongnonpolyaFastqRd1Rep2.fastq.gz ../fastq/wgEncodeCshlLongRnaSeqGm12878NucleusLongnonpolyaFastqRd2Rep2.fastq.gz gm12878.rep2
--- scripts/read.groups.sort.mark.duplicates.sh
+++ scripts/read.groups.sort.mark.duplicates.sh
@@ -6,12 +6,13 @@ b=$(basename $input) ## removes /path/to/file
 filename=${b%.*} ### removes file extension
 random=$RANDOM
 
-gatk FixMisencodedBaseQualityReads -I $input -O $out_dir/$filename.fix.bam
 
-gatk AddOrReplaceReadGroups --java-options "-Xmx30G" -I=$out_dir/$filename.fix.bam -O=$out_dir/$filename.rg.sorted.bam \
+gatk AddOrReplaceReadGroups --java-options "-Xmx30G" -I=$input -O=$out_dir/$filename.rg.sorted.bam \
   --RGID=1 --RGLB=lib1 --RGPL=illumina --RGPU=unit1 --RGSM=$filename -SO=coordinate
 
-gatk MarkDuplicates --java-options "-Xmx30G" -I=$out_dir/$filename.rg.sorted.bam -O=$out_dir/$filename.rg.sorted.markdup.bam \
+gatk FixMisencodedBaseQualityReads -I $filename.rg.sorted.bam -O $out_dir/$filename.rg.sorted.fix.bam
+
+gatk MarkDuplicates --java-options "-Xmx30G" -I=$out_dir/$filename.rg.sorted.fix.bam -O=$out_dir/$filename.rg.sorted.markdup.bam \
   --METRICS_FILE=$filename.metrics.txt --ASSUME_SORT_ORDER=coordinate --REMOVE_DUPLICATES=true --CREATE_INDEX=true
 
 gatk SplitNCigarReads --java-options "-Xmx30G" -I $out_dir/$filename.rg.sorted.markdup.bam -O $out_dir/$filename.rg.sorted.markdup.splitn.bam \
