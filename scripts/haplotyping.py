00707d9d3212d7a3f4b80a5f6c874bce9d1fb2c4
spellmanlab <heskett@ohsu.edu>
2019-03-27 14:07:20 -0700

    D

--- scripts/haplotyping.py
+++ scripts/haplotyping.py
@@ -32,19 +32,17 @@ if __name__ == "__main__":
     data = [[x[0],int(x[1]),int(x[2]),x[3],x[4],int(x[5]),
               int(x[6]),x[7],int(x[8]),int(x[9]),x[10],x[11]] for x in data]
 
-    
+    print(len(data))
+    data = [x if (x[3]==x[10] and x[4]==x[11]) else [ x[0],x[1],x[2],x[4],x[3],x[6],x[5],x[7],x[8],x[9],x[10],x[11] ] for x in data]
+    qc = [x for x in data if (x[3]==x[10] and x[4]==x[11])]
+    qc = [[x[0],x[1],x[2],x[3],x[4],x[5],x[6]] for x in qc]
+    print(len(data))
+    print(len(qc))
 
-    data = [ x[0:6] 
-    if (x[3]==x[10] and x[4]==x[11])
-    else              
-    [x[0],x[1],x[2],x[4],x[3],x[6],x[5]] for x in data 
-    if (x[3]==x[11] and x[4]==x[10]) ]
-
-
-    with open(arguments.out_directory+os.path.basename(arguments.bed).rstrip(".bed")+".haplotype.resolved.bed","w") as i:
-      writer = csv.writer(i,delimiter="\t")
-      writer.writerows(data)
-      i.close()
+    with open(arguments.out_directory+os.path.basename(arguments.bed).rstrip(".bed")+".haplotype.resolved.bed","w") as file:
+      writer = csv.writer(file,delimiter="\t")
+      writer.writerows(qc)
+      file.close()
 
 
 
