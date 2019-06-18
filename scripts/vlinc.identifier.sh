0af1ec3e5d39b9b870c76e31f24403ddb1f7b74a
spellmanlab <heskett@ohsu.edu>
2019-06-18 11:35:47 -0700

    d

--- scripts/vlinc.identifier.sh
+++ scripts/vlinc.identifier.sh
@@ -24,7 +24,7 @@ samtools view -b -f 131 -F 16 $input > $out_dir$filename.fwd1.bam
 samtools index $out_dir$filename.fwd1.bam
 
 # now get first in pair mapping to reverse strand
-samtools view -b -f 83 $out_dir$filename.mq30.bam > $out_dir$filename.fwd2.bam
+samtools view -b -f 83 $input > $out_dir$filename.fwd2.bam
 samtools index $out_dir$filename.fwd2.bam
 
 # now combine, this should contail all plus strand genes
@@ -32,10 +32,10 @@ samtools merge -f $out_dir$filename.plus.bam $out_dir$filename.fwd1.bam $out_dir
 samtools index $out_dir$filename.plus.bam
 
 ## get minus strand
-samtools view -b -f 147 $out_dir$filename.mq30.bam > $out_dir$filename.rev1.bam
+samtools view -b -f 147 $input > $out_dir$filename.rev1.bam
 samtools index $out_dir$filename.rev1.bam
 
-samtools view -b -f 67 -F 16 $out_dir$filename.mq30.bam > $out_dir$filename.rev2.bam
+samtools view -b -f 67 -F 16 $input > $out_dir$filename.rev2.bam
 samtools index $out_dir$filename.rev2.bam
 
 samtools merge -f $out_dir$filename.minus.bam $out_dir$filename.rev1.bam $out_dir$filename.rev2.bam
