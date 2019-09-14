312a4205fccfcc1c34118117ed32d3eca78707e8
mheskett <mbheskett@gmail.com>
2019-09-13 17:15:16 -0700

    d

--- rnbp.of.lines/scripts/rnbp.analyze.py
+++ rnbp.of.lines/scripts/rnbp.analyze.py
@@ -6,6 +6,7 @@ import argparse
 import re
 import matplotlib.pyplot as plt
 import pybedtools
+import scipy.stats
 
 # for the cluster
 # eclip = "/home/groups/Spellmandata/heskett/replication.rnaseq/rnbp.of.lines/big.download/all.eclip.nochr.sorted.bed"
@@ -14,7 +15,7 @@ import pybedtools
 # lines = "/home/groups/Spellmandata/heskett/replication.rnaseq/annotation.files/ucsc.L1.filtered.hg19.bed"
 
 ## for local use
-eclip_file = "/Users/heskett/replication_rnaseq/rnbp.of.lines/data/all.eclip.k562.nochr.sorted.bed"
+eclip_file = "/Users/heskett/replication_rnaseq/rnbp.of.lines/data/all.eclip.hepg2.nochr.sorted.bed"
 introns_file =  "/Users/heskett/replication_rnaseq/annotation.files/ucsc.introns.filtered.hg19.bed"
 vlincs_file = "/Users/heskett/replication_rnaseq/annotation.files/mcaron.vlinc1541.vlinc2149.merged.final.hg19g1k.bed"
 lines_file = "/Users/heskett/replication_rnaseq/annotation.files/ucsc.L1.filtered.hg19.bed"
@@ -25,13 +26,24 @@ vlincs = pybedtools.BedTool(vlincs_file)
 lines = pybedtools.BedTool(lines_file)
 
 
+#### make dict of vlinc length
+f = open(vlincs_file, 'r')
+vlinc_lengths_dict = {}
+for line in f:
+    tmp = line.strip().split('\t')
+    vlinc_lengths_dict[tmp[3]] = int(tmp[2])-int(tmp[1])
+
+f.close()
+################
+
+
 # print("Total number L1s within all vlincs ",len(lines.intersect(vlincs, f=0.9)))
 # print("Total number L1s within all introns ", len(lines.intersect(introns,f=0.9)))
 
 ## strategy is just do a binding per lncRNA approach
 ### eclip database
 ### want to get the total number of peaks per protein
-df_eclip = pd.read_table("/Users/heskett/replication_rnaseq/rnbp.of.lines/data/all.eclip.k562.nochr.sorted.bed",
+df_eclip = pd.read_table("/Users/heskett/replication_rnaseq/rnbp.of.lines/data/all.eclip.hepg2.nochr.sorted.bed",
 					names=["chrom", "start", "stop", "name","score", "strand","signalvalue","pvalue","qvalue","peak"],
 					 dtype={"chrom":str, "start":int, "stop":int,
 					 "name":str, "score":float, "strand":str,
@@ -42,7 +54,7 @@ df_eclip = df_eclip[(~df_eclip.name.str.contains("IDR",case=False))] # total bin
 #plt.hist(df_eclip.name.value_counts().values,bins=50)
 #plt.show()
 eclip_library_size = df_eclip.name.value_counts()
-print(eclip_library_size.sort_index())
+# print(eclip_library_size.sort_index()) # this prints the total number of peaks for each protein
 
 ## could prepare these two files on command line so i dont have to redo it during development 
 # eclip_within_vlincs = eclip.intersect(vlincs,
@@ -68,21 +80,65 @@ eclip_within_vlincs_df = eclip.intersect(vlincs,
 											 "score_eclip":int, "strand_eclip":str,"signalvalue":float,"pvalue":float,"qvalue":float,"peak":float,
 											 "chrom_lnc":str,"start_lnc":int,"stop_lnc":int,"name_lnc":str,"score_lnc":int,"strand_lnc":str})
 
+
+## not sure whether to filter based on significant peaks or all peaks.
 eclip_within_vlincs_df = eclip_within_vlincs_df[(~eclip_within_vlincs_df.name_eclip.str.contains("IDR",case=False)) 
 																& (eclip_within_vlincs_df["pvalue"] >= 2) 
 																& (eclip_within_vlincs_df["signalvalue"] >= 1)]
 eclip_within_vlincs_df = eclip_within_vlincs_df.groupby("name_lnc")["name_eclip"].value_counts().to_frame().rename(columns={"name_eclip":"counts"}).reset_index().pivot(index="name_lnc",columns="name_eclip",values="counts").fillna(value=0)
 
 
-print(eclip_within_vlincs_df)
-print(eclip_within_vlincs_df.divide(eclip_library_size/100, axis="columns")) ## should match the column axis by default. double checked and working
-print(eclip_within_vlincs_df.sum(axis="columns").sort_values(ascending=False))
-print(eclip_within_vlincs_df.sum(axis="columns").describe())
 
-fig,ax = plt.subplots(1,1)
-ax.hist(eclip_within_vlincs_df.sum(axis="columns").values,bins=50)
-ax.set_ylim([0,100])
-plt.show()
+
+#####
+## which one do i filter first?
+
+eclip_within_vlincs_normalized_df = eclip_within_vlincs_df.divide(eclip_library_size/100, axis="columns")
+
+
+print("post filtering pre normalization")
+print(eclip_within_vlincs_normalized_df.sum(axis="columns").sort_values(ascending=False))
+print(eclip_within_vlincs_normalized_df.sum(axis="columns").describe())
+
+print(eclip_within_vlincs_normalized_df.sum(axis="index").sort_values(ascending=False))
+print(eclip_within_vlincs_normalized_df.sum(axis="index").describe())
+
+eclip_within_vlincs_normalized_df = eclip_within_vlincs_normalized_df.loc[eclip_within_vlincs_normalized_df.sum(axis="columns") >= 0.003, ] # filters vlincs ## k562 value 0.02 ## hepg2 value 0.003
+eclip_within_vlincs_normalized_df = eclip_within_vlincs_normalized_df.loc[:, eclip_within_vlincs_normalized_df.sum(axis="index") >= 0.14] # filters proteins ## k562 value 0.34 ## hepg2 value 0.14
+
+################
+
+final_proteins = eclip_within_vlincs_normalized_df.columns
+#check to see if the results correlate with 1) length of ASAR, 2) size of eclip library
+eclip_within_vlincs_normalized_df.loc[:,"col_sum"] = eclip_within_vlincs_normalized_df.sum(axis="columns")
+eclip_within_vlincs_normalized_df.loc[:,"vlinc_length"] = eclip_within_vlincs_normalized_df.apply(lambda x: vlinc_lengths_dict[x.name], axis=1)
+eclip_library_size_final = eclip_library_size[final_proteins]
+### now do correlations
+print(scipy.stats.spearmanr(a=eclip_within_vlincs_normalized_df["vlinc_length"], 
+							b=eclip_within_vlincs_normalized_df["col_sum"])) ## correlation = 0.16 p =.001 not big.
+
+print(eclip_within_vlincs_normalized_df["vlinc_length"])
+print(eclip_within_vlincs_normalized_df["col_sum"])
+
+
+print(scipy.stats.spearmanr(a=eclip_within_vlincs_normalized_df.drop(["col_sum","vlinc_length"],axis=1).sum(axis="index"),b= eclip_library_size_final )) ## SpearmanrResult(correlation=-0.17381298381123095, pvalue=0.08061741938032327)
+print(eclip_within_vlincs_normalized_df.drop(["col_sum","vlinc_length"],axis=1).sum(axis="index")) 
+print(eclip_library_size_final )
+
+eclip_within_vlincs_normalized_df.drop(["col_sum","vlinc_length"],axis=1).sum(axis="columns").sort_values(ascending=False).to_csv("most.bound.lncrnas.hepg2.txt",sep="\t")
+eclip_within_vlincs_normalized_df.drop(["col_sum","vlinc_length"],axis=1).sum(axis="index").sort_values(ascending=False).to_csv("most.bound.rnbps.hepg2.txt",sep="\t")
+
+# fig,ax = plt.subplots(1,1)
+# ax.hist(eclip_vlinc_normalized_df.sum(axis="columns").values,bins=50)
+# ax.set_ylim([0,50])
+# plt.show()
+
+# ## most bound proteins to vlincs
+# fig,ax = plt.subplots(1,1)
+# ax.hist(eclip_vlinc_normalized_df.sum(axis="index").values,bins=50)
+# ax.set_ylim([0,50])
+# plt.show()
+
 
 ##################
 # print("done first step")
