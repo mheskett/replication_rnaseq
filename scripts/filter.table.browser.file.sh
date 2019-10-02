9e956152dab5054be7fcb9af077f0fb7be2c20be
mheskett <mbheskett@gmail.com>
2019-10-01 17:10:25 -0700

    d

--- scripts/filter.table.browser.file.sh
+++ scripts/filter.table.browser.file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed 's/chr//g' $1 | grep -v M | grep -v Y | grep -v fix | grep -v alt | grep -v hap | grep -v random | grep -v 17_ctg | grep -v 4_ctg \
-  | grep -v Un_ | grep -v 6_ssto | grep -v 19_gl | bedtools sort -i stdin -g  /Users/heskett/replication_rnaseq/annotation.files/genome.fa.fai > $2
+sed 's/chr//g' $1 | grep -v "^M" | grep -v "^Y" | grep -v "^fix" | grep -v "^alt" | grep -v "^hap" | grep -v "^random" | grep -v "^17_ctg" | grep -v "^4_ctg" \
+  | grep -v "^Un_" | grep -v "^6_ssto" | grep -v "^19_gl" | bedtools sort -i stdin -g  /Users/heskett/replication_rnaseq/annotation.files/human_g1k_v37.fasta.fai > $2
