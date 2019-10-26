45c60a5175c10677556ef577cfbc8389290b672d
mheskett <mbheskett@gmail.com>
2019-10-25 19:28:56 -0700

    d

--- scripts/allele.rnaseq.given.windows.hg19.py
+++ scripts/allele.rnaseq.given.windows.hg19.py
@@ -85,11 +85,9 @@ def get_tiling_windows(read_counts_file, size):
 	b = b.sort(faidx="/Users/heskett/replication_rnaseq/annotation.files/human_g1k_v37.fasta.fai")
 	c = pybedtools.BedTool()
 
-	window_read_counts = b.map(a=a,b=b,c=[6,7],o="sum",g="/Users/heskett/replication_rnaseq/annotation.files/human_g1k_v37.fasta.fai")
-	df =  window_read_counts.to_dataframe(names=["chrom", "start", "stop", "name", "score", "strand_of_window", 
-										"fraction_l1","hap1_reads", "hap2_reads"],
+	window_read_counts = c.map(a=a, b=b, c=[6,7],o="sum",g="/Users/heskett/replication_rnaseq/annotation.files/human_g1k_v37.fasta.fai")
+	df =  window_read_counts.to_dataframe(names=["chrom", "start", "stop", "hap1_reads", "hap2_reads"],
 										dtype={"chrom":str, "start":int, "stop":int,
-										"name":str, "score":float, "strand_of_window":str, "fraction_l1":float,
 										"hap1_reads":str, "hap2_reads":str})
 	df = df[ (df["hap1_reads"]!=".") & (df["hap2_reads"]!=".") ]
 	df["hap1_reads"] = df["hap1_reads"].astype(int)
@@ -134,7 +132,6 @@ if __name__ == "__main__":
 		help="includ repliseq files if you have them")
 	parser.add_argument("--tiling_windows",
 		required=False,
-		metavar="[tiling windows]",
 		action="store_true")
 ########## funcs
 	def add_binom_pval(df):
@@ -192,25 +189,102 @@ if __name__ == "__main__":
 ###############
 
 	arguments = parser.parse_args()
+	############################
+	###########################
+	############################
+	## tiling window specific program
+
 	if arguments.tiling_windows:
-		df_plus = 
+		df_plus = get_tiling_windows(read_counts_file=arguments.dfplus, size = 50000)
+		df_minus = get_tiling_windows(read_counts_file=arguments.dfminus, size = 50000)
+		print("tiling windows selected")
+		add_binom_pval(df_plus)
+		add_binom_pval(df_minus)
+		df_plus = df_plus[df_plus["chrom"]!="X"]
+		df_minus = df_minus[df_minus["chrom"]!="X"]
 
+		f, ax = plt.subplots(1, len(chromosomes), sharex=False,
+									sharey=False,
+									figsize=(15,1),
+									gridspec_kw={'width_ratios': ratios})
 
-	df_plus = get_windows(window_file = arguments.window_file_plus,
-		read_counts_file=arguments.dfplus)
-	df_minus = get_windows(window_file = arguments.window_file_minus,
-		read_counts_file=arguments.dfminus)
-	add_binom_pval(df_plus)
-	add_binom_pval(df_minus)
-	df_plus = df_plus[df_plus["chrom"]!="X"]
-	df_minus = df_minus[df_minus["chrom"]!="X"]
+	### just want to plot binomial test of 50kb arbitrary windows without expression values necessarily
+		for i in range(len(chromosomes)):
+			if chromosomes[i] not in list(df_plus.chrom):
+				ax[i].set_yticks([])
+				ax[i].set_xticks([])
+				ax[i].margins(x=0,y=0)
 
-	f, ax = plt.subplots(1, len(chromosomes), sharex=False,
-											sharey=False,
-											figsize=(15,1),
-											gridspec_kw={'width_ratios': ratios})
-	for i in range(len(chromosomes)):
-		if chromosomes[i] not in list(df_plus.chrom):
+				# formatting
+				ax[i].set(xlabel=chromosomes[i]) # x axis labels or no
+				ax[i].axvline(x=int(centromere[chromosomes[i]])/10**6, linestyle = "--", lw = 0.5,color="black")
+				ax[i].set_xlim([0,lengths[i]/10**6])
+				continue
+			## clean up this bullcrap
+			#### make a df for each
+			### hap1 plus
+			hap1_plus = df_plus[(df_plus["hap1_reads"] >= df_plus["hap2_reads"]) & (df_plus["chrom"]==chromosomes[i]) ]
+			if len(hap1_plus)>0:
+				hap1_plus["total_reads"] = hap1_plus["hap1_reads"] + hap1_plus["hap2_reads"]
+				hap1_plus["color"] = "green"
+				ax[i].scatter(hap1_plus["start"]/10**6,
+						[-np.log10(x) if ( x >= 10**-20 ) else 20 for x in hap1_plus["binom_pval"]],
+				 		s=10,
+						lw=0.1,
+						edgecolor="black",
+						c=hap1_plus["color"])
+				### text annotation
+				# for index,row in hap1_plus[hap1_plus["fdr_reject"]==True].iterrows():
+				# 	ax[i].annotate(s=row["name"],
+				# 	xy=(row["start"]/10**6, -np.log10(row["binom_pval"]) if -np.log10(row["binom_pval"]) <= 12 else 12)).set_fontsize(6)
+			### hap1 minus
+			hap1_minus = df_minus[(df_minus["hap1_reads"] >= df_minus["hap2_reads"]) & (df_minus["chrom"]==chromosomes[i]) ]
+			if len(hap1_minus)>0:
+				hap1_minus["total_reads"] = hap1_minus["hap1_reads"] + hap1_minus["hap2_reads"]
+				hap1_minus["color"] = "red"
+				ax[i].scatter(hap1_minus["start"]/10**6,
+						[-np.log10(x) if ( x >= 10**-20 ) else 20 for x in hap1_minus["binom_pval"]],
+				 		s=10, # this scaling sucks still
+						lw=0.1,
+						edgecolor="black",
+						c=hap1_minus["color"])
+				### text annotation
+				# for index,row in hap1_minus[hap1_minus["fdr_reject"]==True].iterrows():
+				# 	ax[i].annotate(s=row["name"],
+				# 	xy=(row["start"]/10**6, -np.log10(row["binom_pval"]) if -np.log10(row["binom_pval"]) <= 12 else 12)).set_fontsize(6)
+			### this breaks if the df is empty...thecrappy fix is to assign windows where hap1 reads = hap2 reads to both hap1 and hap2.
+			### it may double plot them, but they will be non significant anyways so who cares?
+			#### hap2 plus
+			hap2_plus = df_plus[(df_plus["hap1_reads"] <= df_plus["hap2_reads"]) & (df_plus["chrom"]==chromosomes[i]) ]
+			if len(hap2_plus)>0:
+				hap2_plus["total_reads"] = hap2_plus["hap1_reads"] + hap2_plus["hap2_reads"]
+				hap2_plus["color"] = "green"
+				ax[i].scatter(hap2_plus["start"]/10**6,
+						[np.log10(x) if ( x >= 10**-20 ) else -20 for x in hap2_plus["binom_pval"]],
+				 		s=10,
+						lw=0.1,
+						edgecolor="black",
+						c=hap2_plus["color"])
+				### annotation by text. doesnt show up well.
+				# for index,row in hap2_plus[hap2_plus["fdr_reject"]==True].iterrows():
+				# 	ax[i].annotate(s=row["name"],
+				# 	xy=(row["start"]/10**6, -1*(-np.log10(row["binom_pval"]) if -np.log10(row["binom_pval"]) <= 12 else 12))).set_fontsize(6)
+			### hap2 minus
+			hap2_minus = df_minus[(df_minus["hap1_reads"] <= df_minus["hap2_reads"]) & (df_minus["chrom"]==chromosomes[i]) ]
+			if len(hap2_minus)>0:
+				hap2_minus["total_reads"] = hap2_minus["hap1_reads"] + hap2_minus["hap2_reads"]
+				hap2_minus["color"] = "red"
+				ax[i].scatter(hap2_minus["start"]/10**6,
+						[np.log10(x) if ( x >= 10**-20 ) else -20 for x in hap2_minus["binom_pval"]],
+				 		s=10,
+						lw=0.1,
+						edgecolor="black",
+						c=hap2_minus["color"])
+				### annotation by text. doesnt show up well.
+				# for index,row in hap2_minus[hap2_minus["fdr_reject"]==True].iterrows():
+				# 	ax[i].annotate(s=row["name"],
+				# 	xy=(row["start"]/10**6, -1*(-np.log10(row["binom_pval"]) if -np.log10(row["binom_pval"]) <= 12 else 12))).set_fontsize(6)
+
 			ax[i].set_yticks([])
 			ax[i].set_xticks([])
 			ax[i].margins(x=0,y=0)
@@ -219,7 +293,44 @@ if __name__ == "__main__":
 			ax[i].set(xlabel=chromosomes[i]) # x axis labels or no
 			ax[i].axvline(x=int(centromere[chromosomes[i]])/10**6, linestyle = "--", lw = 0.5,color="black")
 			ax[i].set_xlim([0,lengths[i]/10**6])
-			continue
+			#ax[i].set_yticks([-12,-8,-4,0,4,8,12])
+			#plt.grid(True)
+			ax[i].set_ylim([-21,21]) # for all plots
+		for i in range(len(chromosomes)):
+			if chromosomes[i] in gray_chromosomes:
+			    ax[i].axvspan(xmin=0, xmax=lengths[i]/10**6, ymin=0, ymax=1,
+			     alpha=0.2,facecolor="gray") 
+
+
+    ####################
+    ####################
+    ####################
+	####################
+	else:
+		df_plus = get_windows(window_file = arguments.window_file_plus,
+			read_counts_file=arguments.dfplus)
+		df_minus = get_windows(window_file = arguments.window_file_minus,
+			read_counts_file=arguments.dfminus)
+		add_binom_pval(df_plus)
+		add_binom_pval(df_minus)
+		df_plus = df_plus[df_plus["chrom"]!="X"]
+		df_minus = df_minus[df_minus["chrom"]!="X"]
+
+		f, ax = plt.subplots(1, len(chromosomes), sharex=False,
+											sharey=False,
+											figsize=(15,1),
+											gridspec_kw={'width_ratios': ratios})
+		for i in range(len(chromosomes)):
+			if chromosomes[i] not in list(df_plus.chrom):
+				ax[i].set_yticks([])
+				ax[i].set_xticks([])
+				ax[i].margins(x=0,y=0)
+
+				# formatting
+				ax[i].set(xlabel=chromosomes[i]) # x axis labels or no
+				ax[i].axvline(x=int(centromere[chromosomes[i]])/10**6, linestyle = "--", lw = 0.5,color="black")
+				ax[i].set_xlim([0,lengths[i]/10**6])
+				continue
 		## clean up this bullcrap
 		#### make a df for each
 		### hap1 plus
@@ -300,37 +411,41 @@ if __name__ == "__main__":
 		#ax[i].set_yticks([-12,-8,-4,0,4,8,12])
 		#plt.grid(True)
 		ax[i].set_ylim([-14,14]) # for all plots
-	for i in range(len(chromosomes)):
-		if chromosomes[i] in gray_chromosomes:
-		    ax[i].axvspan(xmin=0, xmax=lengths[i]/10**6, ymin=0, ymax=1,
-		     alpha=0.2,facecolor="gray") 
-
-	if arguments.repli_seq:
-		df_repliseq = pd.read_csv(arguments.repli_seq,sep="\t",names=["chrom","start","stop","pvalue"],
-													dtype={"chrom":str,"start":int,"stop":int,"pvalue":float})
 		for i in range(len(chromosomes)):
-			df_repliseq_chr = df_repliseq[(df_repliseq["chrom"]=="chr"+str(chromosomes[i])) &
-											(df_repliseq["pvalue"] <= 0.01 )]
-			print(df_repliseq_chr)								
-			print("chr ",chromosomes[i])								
-			print("plotting all the asynchronous sites")
-			for index,row in df_repliseq_chr.iterrows():
-				ax[i].axvspan(xmin=row["start"]/10**6-0.1, 
-					xmax=row["stop"]/10**6+0.1,
-					ymin=0,ymax=1,
-					alpha=0.8,facecolor="blue")
+			if chromosomes[i] in gray_chromosomes:
+			    ax[i].axvspan(xmin=0, xmax=lengths[i]/10**6, ymin=0, ymax=1,
+			     alpha=0.2,facecolor="gray") 
+
+		if arguments.repli_seq:
+			df_repliseq = pd.read_csv(arguments.repli_seq,sep="\t",names=["chrom","start","stop","pvalue"],
+														dtype={"chrom":str,"start":int,"stop":int,"pvalue":float})
+			for i in range(len(chromosomes)):
+				df_repliseq_chr = df_repliseq[(df_repliseq["chrom"]=="chr"+str(chromosomes[i])) &
+												(df_repliseq["pvalue"] <= 0.01 )]
+				print(df_repliseq_chr)								
+				print("chr ",chromosomes[i])								
+				print("plotting all the asynchronous sites")
+				for index,row in df_repliseq_chr.iterrows():
+					ax[i].axvspan(xmin=row["start"]/10**6-0.1, 
+						xmax=row["stop"]/10**6+0.1,
+						ymin=0,ymax=1,
+						alpha=0.8,facecolor="blue")
 
 	print("done plotting")
 	plt.subplots_adjust(wspace=0, hspace=0)
-	plt.savefig(arguments.out_directory+os.path.basename(arguments.dfplus.replace(".plus.overlap.platinum.haplotypes.bed",""))+"."+os.path.basename(arguments.window_file_plus.replace(".bed","")+".png"),
-		dpi=400,transparent=True,bbox_inches='tight',pad_inches = 0)
+
+	if arguments.tiling_windows:
+		out_string = arguments.out_directory+os.path.basename(arguments.dfplus.replace(".plus.overlap.platinum.haplotypes.bed",""))+"."+"tiling"
+	else: 
+		out_string = arguments.out_directory+os.path.basename(arguments.dfplus.replace(".plus.overlap.platinum.haplotypes.bed",""))+"."+os.path.basename(arguments.window_file_plus).replace(".bed","").replace("plus","")
+
+	plt.savefig(out_string+".png", dpi=400, transparent=True, bbox_inches='tight', pad_inches = 0)
 
 	combined_df = pd.concat([df_plus[df_plus["binom_pval"] <= 0.000001], df_minus[df_minus["binom_pval"] <= 0.000001]])
-	combined_df.to_csv(os.path.basename(arguments.dfplus.replace(".plus.overlap.platinum.haplotypes.bed",""))+"."+os.path.basename(arguments.window_file_plus.replace(".bed",""))+".bed",
-		sep="\t",index=None,header=None)
-
-	### output files with proper filenames
-	browser_df = combined_df[combined_df["binom_pval"] <= 0.000001].loc[:,:"strand_of_window"]
-	browser_df["chrom"] = "chr"+browser_df["chrom"].astype(str)
-	browser_df.to_csv(os.path.basename(arguments.dfplus.replace(".plus.overlap.platinum.haplotypes.bed",""))+"."+os.path.basename(arguments.window_file_plus.replace(".bed",""))+".browser.bed",
-		sep="\t",index=None,header=None)
+	combined_df.to_csv(out_string + ".bed", sep="\t", index=None, header=None)
+	if not arguments.tiling_windows:
+		### dont need to make genome browser files for just random windows
+		### output files with proper filenames
+		browser_df = combined_df[combined_df["binom_pval"] <= 0.000001].loc[:,:"strand_of_window"]
+		browser_df["chrom"] = "chr"+browser_df["chrom"].astype(str)
+		browser_df.to_csv(out_string + ".browser.bed", sep="\t",index=None,header=None)
