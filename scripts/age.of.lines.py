7a301a70fa57ecc6dbe310cc20b6cc8dc90e42b6
mheskett <mbheskett@gmail.com>
2019-11-13 18:35:13 -0800

    d

---
+++ scripts/age.of.lines.py
@@ -0,0 +1,80 @@
+import os
+import re
+import csv
+import numpy as np
+import pandas as pd
+import argparse
+import re
+import seaborn as sns
+import scipy.stats
+import matplotlib.pyplot as plt
+import pybedtools
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
+                               AutoMinorLocator)
+import statsmodels.stats.multitest as mt
+
+def get_age(x,age_dict):
+	return age_dict[x] if x in age_dict.keys() else get_age(x[:-1],age_dict)
+
+line_age = pd.read_csv("/Users/heskett/replication_rnaseq/annotation.files/warburton.line.age.ranking.txt",
+						names=["line_name","evolutionary_age_ranking"],sep="\t")
+all_ucsc_lines = pd.read_csv("/Users/heskett/replication_rnaseq/annotation.files/all.l1.elements.txt",header=None,sep="\t",names=["line_name"])
+
+all_lines_df = pd.read_csv("/Users/heskett/replication_rnaseq/annotation.files/ucsc.L1.filtered.hg19.bed",sep="\t",names=["chrom","start","stop","name","score","strand"],
+							header=None)
+asars_file = "/Users/heskett/replication_rnaseq/scripts/gm12878.rep1.hg19Aligned.outgm12878.rep1.expressed.vlincs.skewed.bed"
+lines_file = "/Users/heskett/replication_rnaseq/annotation.files/ucsc.L1.filtered.hg19.bed"
+all_vlincs_file = "/Users/heskett/replication_rnaseq/scripts/gm12878.rep1.hg19Aligned.outgm12878.rep1.expressed.vlincs.all.bed"
+asars = pybedtools.BedTool(asars_file)
+lines = pybedtools.BedTool(lines_file)
+all_vlincs = pybedtools.BedTool(all_vlincs_file)
+df = asars.intersect(lines,wa=True,wb=True).\
+					to_dataframe(names=["chrom","start","stop","name_lncrna","rpkm","strand","l1_fraction","hap1_reads","hap2_reads",
+														"binom_pval","fdr_pval","fdr_reject","total_reads","skew","chrom_line",
+														"start_line","stop_line","name_line","score_line","strand_line"],
+														dtype={"chrom":str,"start":int,"stop":int,"name_lncrna":str,"rpkm":float,"strand":str,"l1_fraction":float,"hap1_reads":int,"hap2_reads":int,
+														"binom_pval":float,"fdr_pval":float,"fdr_reject":str,"total_reads":int,"skew":float,"chrom_line":str,
+														"start_line":int,"stop_line":int,"name_line":str,"score_line":float,"strand_line":str})
+df_all = all_vlincs.intersect(lines,wa=True,wb=True).\
+					to_dataframe(names=["chrom","start","stop","name_lncrna","rpkm","strand","l1_fraction","hap1_reads","hap2_reads",
+														"binom_pval","fdr_pval","fdr_reject","total_reads","skew","chrom_line",
+														"start_line","stop_line","name_line","score_line","strand_line"],
+														dtype={"chrom":str,"start":int,"stop":int,"name_lncrna":str,"rpkm":float,"strand":str,"l1_fraction":float,"hap1_reads":int,"hap2_reads":int,
+														"binom_pval":float,"fdr_pval":float,"fdr_reject":str,"total_reads":int,"skew":float,"chrom_line":str,
+														"start_line":int,"stop_line":int,"name_line":str,"score_line":float,"strand_line":str})
+
+df_not_skewed = df_all[(df_all["binom_pval"]>10**-6) | (df_all["skew"]<0.25)]
+
+age_dict = dict(zip(line_age.line_name, line_age.evolutionary_age_ranking))
+
+
+###
+all_lines_df.loc[:,"line_age"] = all_lines_df.apply(lambda x: get_age(x["name"], age_dict),axis=1)
+print(all_lines_df)
+all_lines_df.to_csv("ucsc.L1.filtered.age.hg19.bed",sep="\t",header=None,index=None)
+###
+df.loc[:,"line_age"] = df.apply(lambda x: get_age(x["name_line"], age_dict),axis=1) # higher number is younger age
+df_not_skewed.loc[:,"line_age"] = df_not_skewed.apply(lambda x: get_age(x["name_line"], age_dict),axis=1) # higher number is younger age
+
+sns.set(font_scale=1.5)
+
+fig, ax = plt.subplots()
+sns.kdeplot(df.groupby("name_lncrna")["line_age"].mean(),shade=True,label="LINE_age_score_skewed")
+# sns.kdeplot(df_not_skewed.groupby("name_lncrna")["line_age"].mean(),shade=True,label="LINE_age_score_biallelic")
+
+# ax.set_ylim([0,0.19])
+# ax.set_xlim([-2,30])
+plt.show()
+plt.close()
+
+# df = df.loc[:,"line_age"] = df.apply(lambda x: age_dict[x["name_line"]],axis=1)
+# print(df)
+## some strategies
+## 1. create "line age" score where you calculate the average (or whatever metric) evolutionary age of all the lines in an ASAR
+## 2. check to see if there's more of any particular L1 subtype than you would expect based on random sampling of all of them
+## based on (warburton et al plos comp bio)
+## 3. Determine if ASAR L1PA2s have shared features such as deletions, substitutions etc so for this you could cluster all L1PA2s
+## and label by within an ASAR or not within an ASAR
+## 4. cluster ASARs based on L1 composition -- the relative frequency of each LINE can be the features.
+
+
--- scripts/cluster.vlinc.features.py
+++ scripts/cluster.vlinc.features.py
@@ -12,102 +12,180 @@ import pybedtools
 from sklearn import metrics
 from sklearn.cluster import DBSCAN
 from mpl_toolkits.mplot3d import Axes3D
-
 from sklearn.decomposition import PCA
-
 from sklearn import manifold, datasets
+###
 
-# df_plus = pd.read_csv("/Users/heskett/replication_rnaseq/data/gm12878.rep1.hg19Aligned.out.samtool.rmdup.plus.1000.10000.50000.vlinc.discovery.bed",
-# 				header=None,
-# 				index_col=None,
-# 				names=["chrom","start","stop","name","reads/kb","strand","fraction_l1"],
-# 				sep="\t")
-
-# df_minus = pd.read_csv("/Users/heskett/replication_rnaseq/data/gm12878.rep1.hg19Aligned.out.samtool.rmdup.minus.1000.10000.50000.vlinc.discovery.bed",
-# 				header=None,
-# 				index_col=None,
-# 				names=["chrom","start","stop","name","reads/kb","strand","fraction_l1"],
-# 				sep="\t")
-df = pd.read_csv("/Users/heskett/replication_rnaseq/scripts/gm12878.rep1.hg19Aligned.out.gm12878.rep1.hg19Aligned.out.samtool.rmdup.plus.1000.10000.50000.vlinc.discovery.bed",
-				header=None,
-				index_col=None,
-				names=["chrom","start","stop","name","reads/kb","strand","fraction_l1","hap1_reads","hap2_reads","pval","qval","reject"],
-				sep="\t")
+###
 
 
-#df = df[df["chrom"]!="X"]
-# df = pd.concat([df_plus,df_minus])
-df = df[df["chrom"]!="X"]
-df.loc[:,"length"] = df["stop"] - df["start"]
-df.loc[:,"length_zscore"] = scipy.stats.mstats.zscore(df["length"])
-df.loc[:,"reads/kb_zscore"] = scipy.stats.mstats.zscore(df["reads/kb"])
-df.loc[:,"fraction_l1_zscore"] = scipy.stats.mstats.zscore(df["fraction_l1"])
+all_vlincs = pybedtools.BedTool("gm12878.rep2.hg19Aligned.outgm12878.rep2.expressed.vlincs.all.bed").sort(g="/Users/heskett/replication_rnaseq/annotation.files/human_g1k_v37.fasta.fai")
+line_age = pybedtools.BedTool("/Users/heskett/replication_rnaseq/annotation.files/ucsc.L1.filtered.age.hg19.bed").sort(g="/Users/heskett/replication_rnaseq/annotation.files/human_g1k_v37.fasta.fai")
 
-# print(df["reads/kb"].describe())
-# print(df["length"].describe())
-# print(df["fraction_l1"].describe())
+## now do a bedtool map with lines and -c mean
+df = all_vlincs.map(line_age, c=7 ,o="mean", g="/Users/heskett/replication_rnaseq/annotation.files/human_g1k_v37.fasta.fai")\
+				.to_dataframe(names=["chrom","start","stop","name","rpkm","strand","l1_fraction","hap1_reads","hap2_reads",
+	"binom_pval","fdrpval","reject","total_reads","skew","average_line_age"],
+	dtype={"chrom":str,"start":int,"stop":int,"name":str,"rpkm":float,"strand":str,"l1_fraction":float,"hap1_reads":int,"hap2_reads":int,
+	"binom_pval":float,"fdrpval":float,"reject":str,"total_reads":int,"skew":float,"average_line_age":str} )
 
-# df.hist(["length","reads/kb","fraction_l1"],bins=30)
-# plt.show()
-
-# plt.hist(df["fraction_l1"],range=(0,0.8),bins=30)
-# plt.show()
-# plt.close()
-# plt.hist(df["length"],range = (50000,300000),bins=30)
-# plt.show()
-# plt.close()
-# plt.hist(df["reads/kb"], range = (0,100),bins=30)
-# plt.show()
-# plt.close()
-
-
+df=df[df["average_line_age"]!="."]
+df.loc[:,"average_line_age"] = df["average_line_age"].astype(float)
 print(df)
 
-print(df["reads/kb_zscore"].describe())
-print(df["length_zscore"].describe())
-print(df["fraction_l1_zscore"].describe())
-X = df.loc[:,["reads/kb_zscore","length_zscore","fraction_l1_zscore"]]
+# df = pd.read_csv("gm12878.rep1.hg19Aligned.outgm12878.rep1.expressed.vlincs.all.bed",
+# 	sep="\t",header=None,
+# 	names=["chrom","start","stop","name","rpkm","strand","l1_fraction","hap1_reads","hap2_reads",
+# 	"binom_pval","fdrpval","reject","total_reads","skew"],
+# 	dtype={"chrom":str,"start":int,"stop":int,"name":str,"rpkm":float,"strand":str,"l1_fraction":float,"hap1_reads":int,"hap2_reads":int,
+# 	"binom_pval":float,"fdrpval":float,"reject":str,"total_reads":int,"skew":float})
+
+
+df.loc[:,"length"] = df["stop"] - df["start"]
+df.loc[:,"length_zscore"] = scipy.stats.mstats.zscore(df["length"])
+df.loc[:,"reads/kb_zscore"] = scipy.stats.mstats.zscore(df["rpkm"])
+df.loc[:,"fraction_l1_zscore"] = scipy.stats.mstats.zscore(df["l1_fraction"])
+df.loc[:,"skew_zscore"] = scipy.stats.mstats.zscore(abs(df["skew"]))
+df.loc[:,"average_line_age_zscore"] = scipy.stats.mstats.zscore(df["average_line_age"])
+
+xist = df[df["name"]=="3438_gm12878.rep1.hg19Aligned.out.samtool.rmdup"].reset_index(drop=True)
+df = df[df["chrom"]!="X"]
+df_notskewed = df[(df["binom_pval"]>10**-6) | (abs(df["skew"])>0.4)].reset_index(drop=True)
+df_skewed = df[(df["binom_pval"]<=10**-6) & (abs(df["skew"])>=0.4)].reset_index(drop=True)
+
+# sns.set(font_scale=1.5)  # crazy big
+# fig, ax = plt.subplots()
+# sns.kdeplot(df_notskewed["l1_fraction"],shade=True,label="biallelic_vlincs")
+# sns.kdeplot(df_skewed["l1_fraction"],shade=True,label="skewed_vlincs")
+# ax.set_xlim([0, 1])
+# ax.set_ylim([0, 5])
+# plt.show()
+# plt.close()
+
+
+# fig, ax = plt.subplots()
+# sns.set(font_scale=1.5)  # crazy big
+
+# sns.kdeplot(df_notskewed["length"],shade=True,label="biallelic_vlincs")
+# sns.kdeplot(df_skewed["length"],shade=True,label="skewed_vlincs")
+# ax.set_xlim([0, 1500000])
+# plt.show()
+# plt.close()
+# sns.set(font_scale=1.5)  # sns before fig,ax gibves you seaborn colors. fig ax before sns give syou matplotlib colors
+# fig, ax = plt.subplots()
+# sns.kdeplot(df_notskewed["rpkm"],shade=True,label="biallelic_vlincs")
+# sns.kdeplot(df_skewed["rpkm"],shade=True,label="skewed_vlincs")
+# ax.set_xlim([0, 300])
+
+# plt.show()
+# plt.close()
+
+
+### this is doing all of them
+
+X = df.loc[:,["reads/kb_zscore","length_zscore","fraction_l1_zscore","skew_zscore","average_line_age_zscore"]]
 reduced_data = PCA(n_components=2).fit_transform(X.values)
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df["reads/kb_zscore"],cmap="Reds",vmin=-2,vmax=2)
+ax = fig.add_subplot(151)
+ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df["rpkm"],cmap="Reds",vmin=1,vmax=150)
+
+ax = fig.add_subplot(152)
+ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df["length"],cmap="Reds",vmin=50*10**3,vmax=500*10**3)
+
+ax = fig.add_subplot(153)
+ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df["l1_fraction"],cmap="Reds",vmin=0.05,vmax=0.4)
+
+ax = fig.add_subplot(154)
+ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df["skew"],cmap="Reds",vmin=0,vmax=1)
+
+ax = fig.add_subplot(155)
+ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df["average_line_age"],cmap="Reds",vmin=15,vmax=200)
+
 plt.show()
 plt.close()
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df["length_zscore"],cmap="Reds",vmin=-2,vmax=2)
-plt.show()
-plt.close()
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df["fraction_l1_zscore"],cmap="Reds",vmin=-2,vmax=2)
-plt.show()
-plt.close()
-
-
 
 print("computing tsne")
 tsne = manifold.TSNE(n_components=2, init='random',
                          random_state=0).fit_transform(X.values)
 
 fig = plt.figure()
-ax = fig.add_subplot(131)
-ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black",c=df["reads/kb_zscore"],cmap="Reds",vmin=-2,vmax=2)
-# plt.show()
-# plt.close()
-# fig = plt.figure()
-ax = fig.add_subplot(132)
-ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black",c=df["fraction_l1_zscore"],cmap="Reds",vmin=-2,vmax=2)
-# plt.show()
-# plt.close()
-# fig = plt.figure()
-ax = fig.add_subplot(133)
-ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black",c=df["length_zscore"],cmap="Reds",vmin=-2,vmax=2)
+ax = fig.add_subplot(151)
+ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black", c=df["rpkm"], cmap="Reds", vmin=1, vmax=150)
+
+ax = fig.add_subplot(152) 
+ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black", c=df["length"], cmap="Reds", vmin=50*10**3, vmax=500*10**3)
+
+ax = fig.add_subplot(153)
+ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black", c=df["l1_fraction"], cmap="Reds", vmin=0.05, vmax=0.4
+
+ax = fig.add_subplot(154)
+ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black", c=df["skew"],cmap="Reds", vmin=0,vmax=1)
+
+ax = fig.add_subplot(155)
+ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black", c=df["average_line_age"], cmap="Reds",vmin=15,vmax=200)
+
 plt.show()
 plt.close()
+
+#### now do this for just the skewed ones and add XIST
+df_skewed = df_skewed.append(xist)
+print(df_skewed)
+X = df_skewed.loc[:,["reads/kb_zscore","length_zscore","fraction_l1_zscore","skew_zscore","average_line_age_zscore"]]
+reduced_data = PCA(n_components=2).fit_transform(X.values)
+
+fig = plt.figure()
+ax = fig.add_subplot(151)
+ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df_skewed["rpkm"],cmap="Reds",vmin=-2,vmax=2)
+
+ax = fig.add_subplot(152)
+ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df_skewed["length"],cmap="Reds",vmin=-2,vmax=2)
+
+ax = fig.add_subplot(153)
+ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df_skewed["l1_fraction"],cmap="Reds",vmin=-2,vmax=2)
+
+ax = fig.add_subplot(154)
+ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df_skewed["skew"],cmap="Reds",vmin=-2,vmax=2)
+
+ax = fig.add_subplot(155)
+ax.scatter(x=reduced_data[:,0],y=reduced_data[:,1],lw=0.1,edgecolor="black",c=df_skewed["average_line_age"],cmap="Reds",vmin=-2,vmax=2)
+
+
+plt.show()
+plt.close()
+
+print("computing tsne")
+tsne = manifold.TSNE(n_components=2, init='random',
+                         random_state=0).fit_transform(X.values)
+
+fig = plt.figure()
+ax = fig.add_subplot(151)
+ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black",c=df_skewed["rpkm"],cmap="Reds",vmin=-2,vmax=2)
+ax.scatter(x=tsne[-1,0],y=tsne[-1,1],lw=0.1,edgecolor="black",c=xist["rpkm"],cmap="Reds",s=200,vmin=-2,vmax=2)
+
+
+ax = fig.add_subplot(152)
+ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black",c=df_skewed["l1_fraction"],cmap="Reds",vmin=-2,vmax=2)
+ax.scatter(x=tsne[-1,0],y=tsne[-1,1],lw=0.1,edgecolor="black",c=xist["l1_fraction"],cmap="Reds",vmin=-2,vmax=2,s=200)
+
+
+ax = fig.add_subplot(153)
+ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black",c=df_skewed["length"],cmap="Reds",vmin=-2,vmax=2)
+ax.scatter(x=tsne[-1,0],y=tsne[-1,1],lw=0.1,edgecolor="black",c=xist["length"],cmap="Reds",vmin=-2,vmax=2,s=200)
+
+
+ax = fig.add_subplot(154)
+ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black",c=df_skewed["skew"],cmap="Reds",vmin=-2,vmax=2)
+ax.scatter(x=tsne[-1,0],y=tsne[-1,1],lw=0.1,edgecolor="black",c=xist["skew"],cmap="Reds",vmin=-2,vmax=2,s=200)
+
+ax = fig.add_subplot(155)
+ax.scatter(x=tsne[:,0],y=tsne[:,1],lw=0.1,edgecolor="black",c=df_skewed["average_line_age"],cmap="Reds",vmin=-2,vmax=2)
+ax.scatter(x=tsne[-1,0],y=tsne[-1,1],lw=0.1,edgecolor="black",c=xist["average_line_age"],cmap="Reds",vmin=-2,vmax=2,s=200)
+
+
+plt.show()
+plt.close()
+
 
 
 ###make plot with x is length zscore, y is reads/kb z score, color is red for high L1, blue for low L1
