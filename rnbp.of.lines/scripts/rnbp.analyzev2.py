e8d1361b9b193d7b69c42909cdd85ab14c5e95c9
mheskett <mbheskett@gmail.com>
2019-11-17 14:55:57 -0800

    almost there

--- rnbp.of.lines/scripts/rnbp.analyzev2.py
+++ rnbp.of.lines/scripts/rnbp.analyzev2.py
@@ -10,49 +10,45 @@ import scipy.stats
 import seaborn as sns
 import multiprocessing as mp
 
-def sample_introns(length):
-	# creates a df of introns where the sum of lengths equals length
-	tmp = df_introns.sample(n=1)
-	while tmp["intron_length"].sum() < length:
-		tmp = pd.concat([tmp,df_introns.sample(n=1)], ignore_index=True)
-	remainder = tmp["intron_length"].sum() - length
 
+def line_sampler(df_lines_in_introns_coverage, eclip, length):
+	tmp = df_lines_in_introns_coverage.sample(n=1)
+	while tmp["line_length"].sum() < length:
+		tmp = pd.concat( [tmp, df_lines_in_introns_coverage.sample(n=1)], ignore_index = True)
+	remainder = tmp["line_length"].sum() - length
 	if remainder > 0:
-		# print(tmp)
-		###
 		last_row = tmp.tail(1).reset_index(drop=True)
 		remainder_start = last_row.at[0, "start"]
 		remainder_stop = last_row.at[0, "stop"]
 		remainder_chrom = last_row.at[0, "chrom"]
-		remainder_intron_name = last_row.at[0, "intron_name"]
+		remainder_line_name = last_row.at[0, "line_name"]
 		remainder_score = 0
 		remainder_strand = last_row.at[0, "strand"]
 		new_remainder_stop = remainder_stop - remainder
-
-		bedtool_string = str(remainder_chrom)+' '+str(remainder_start)+' '+str(new_remainder_stop) + ' ' + str(remainder_intron_name) + ' ' + str(remainder_score)+ ' ' + str(remainder_strand) 
+		bedtool_string = str(remainder_chrom) + ' ' + str(remainder_start) + ' ' + str(new_remainder_stop) + ' ' + str(remainder_line_name) + ' ' + str(remainder_score)+ ' ' + str(remainder_strand) 
 		# convert new intron to bed tool, get line counts
 		try:
-			new_intron_fragment = pybedtools.BedTool(bedtool_string, from_string=True)
+			new_line_fragment = pybedtools.BedTool(bedtool_string, from_string=True)
 			# print(bedtool_string)
 		except:
 			print(bedtool_string)
 			print(tmp)
 			print("ERROR...handling error")
 			return sample_introns(length) # this could go infite loop if errors keep happening, but since they are random i dont think it can
-		new_intron_fragment_eclip = new_intron_fragment.coverage(eclip, F=0.9).to_dataframe(names=["chrom", "start", "stop", "intron_name","score",
+		new_line_fragment_coverage = new_line_fragment.coverage(eclip, F=0.5).to_dataframe(names=["chrom", "start", "stop", "line_name","score",
 																									"strand","eclip_count", "num_bases_covered",
-																									"intron_length", "fraction_bound"],
-																							dtype={"chrom":str,"start":int,"stop":int,"intron_name":str,
+																									"line_length", "fraction_bound"],
+																							dtype={"chrom":str,"start":int,"stop":int,"line_name":str,
 																							"score":int,"strand":str,"eclip_count":int,
-																							"num_bases_covered":int,"intron_length":int,"fraction_bound":float})
+																							"num_bases_covered":int,"line_length":int,"fraction_bound":float})
 		tmp = tmp.drop(tmp.tail(1).index)
-		tmp = pd.concat([tmp,new_intron_fragment_eclip], ignore_index=True)
+		tmp = pd.concat([tmp,new_line_fragment_coverage], ignore_index=True)
 
-	sum_intron_length = tmp["intron_length"].sum()
+	sum_line_length = tmp["line_length"].sum()
 	sum_eclip_counts = tmp["eclip_count"].sum()
-	peaks_per_length = sum_eclip_counts / sum_intron_length
-
-	if sum_intron_length != length:
+	peaks_per_length = sum_eclip_counts / sum_line_length
+	print(tmp)
+	if sum_line_length != length:
 		print("DEBUG ALERT")
 		print(tmp)
 		print("sum intron length ", tmp["intron_length"].sum())
@@ -60,30 +56,27 @@ def sample_introns(length):
 		print("remainder: ", remainder)
 		print(bedtool_string)
 	if peaks_per_length == 0:
-		return 1 / sum_intron_length
+		return 1 / sum_line_length
 
 	return peaks_per_length
 
-def get_pvalue_eclip_peaks(vlinc_chrom, vlinc_start, vlinc_stop, vlinc_name, vlinc_length, peaks_per_length, num_simulations):
+
+def get_pvalue_eclip_peaks(length, rnbp_name, peaks_per_length_observed, num_simulations,df_lines_in_introns_coverage,eclip):
+	## going to do this once for each protein
 	results = []
 	for i in range(0, num_simulations):
-		results += [sample_introns(length = vlinc_length)]
+		results += [line_sampler(length = length)]
 	### fit beta to results
 	a1,b1,loc1,scale1 = scipy.stats.beta.fit(results, floc=0, fscale=1)
 	x = np.linspace(0,1,1000)
 	y = scipy.stats.beta.pdf(x, a1, b1)
-	beta_pvalue = 1 - scipy.stats.beta.cdf(peaks_per_length, a1, b1)
+	beta_pvalue = 1 - scipy.stats.beta.cdf(peaks_per_length_observed, a1, b1)
 	# plt.plot(x, y)
 	# plt.show()
 	# plt.hist(results,bins=20)
 	# plt.show()
-	# pvalue = (100 - scipy.stats.percentileofscore(results, line_fraction, kind='strict')) / 100
-	# if pvalue == 0: # beta dist is better than this i think
-	# 	pvalue = 1/num_simulations
-	print(vlinc_name, "done")
-
-	### or center and scale data then fit a normal, or fit a poisson or whatever
-	return vlinc_chrom, vlinc_start, vlinc_stop, vlinc_name, line_fraction, beta_pvalue
+	print(rnbp_name, "done")
+	return rnbp_name, peaks_per_length_observed, beta_pvalue
 
 
 ## the main question is:
@@ -110,6 +103,8 @@ df_eclip = pd.read_csv(eclip_file,sep="\t",
 df_eclip = df_eclip[ (~df_eclip.name.str.contains("IDR",case=False)) & (df_eclip["log_fold_enrichment"] >= 1) & (df_eclip["log_pvalue"] >= 2) ] # filter out replication experiments
 df_eclip = df_eclip.loc[:,["chrom", "start", "stop", "name","score", "strand", "log_fold_enrichment","log_pvalue"]]
 eclip_experiments = list( df_eclip.name.unique() ) ## each protein is separate
+eclip = pybedtools.BedTool.from_dataframe(df_eclip)
+
 #################
 df_vlincs = pd.read_csv(skewed_vlincs,sep="\t",names=["chrom","start","stop","name_lncrna","rpkm","strand","l1_fraction","hap1_reads","hap2_reads",
 																"binom_pval","fdr_pval","fdr_reject","total_reads","skew"],
@@ -119,7 +114,6 @@ vlincs = pybedtools.BedTool.from_dataframe(df_vlincs.loc[:,["chrom","start","sto
 ################
 lines_in_vlincs = lines.intersect(vlincs,f=0.5,wa=True)
 ###############
-eclip = pybedtools.BedTool.from_dataframe(df_eclip)
 binding_in_vlincs = eclip.intersect(vlincs,f=0.5,wa=True,s=True,wb=True) # this binding does need to be stranded
 ## this is all eclip within vlincs
 df_binding_in_vlincs = binding_in_vlincs.to_dataframe(names=["chrom_eclip", "start_eclip", "stop_eclip", "name_eclip","score_eclip", "strand_eclip", "log_fold_enrichment","log_pvalue",
@@ -129,9 +123,26 @@ df_binding_in_vlincs = binding_in_vlincs.to_dataframe(names=["chrom_eclip", "sta
 													"chrom_vlinc":str,"start_vlinc":int, "stop_vlinc":int,"name_vlinc":str,"rpkm":float,"strand_vlinc":str,"hap1_reads":int,"hap2_reads":int,"binom_pval":float,"skew":float})
 ## this is all eclip within lines within vlincs
 binding_in_lines_in_vlincs = lines_in_vlincs.intersect(binding_in_vlincs,wa=True,wb=True)
-
-print(len(binding_in_lines_in_vlincs))
-
+df_binding_in_lines_in_vlincs = binding_in_lines_in_vlincs.to_dataframe(names = ["chrom_line","start_line","stop_line","name_line","score_line","strand_line",
+																				"chrom_eclip","start_eclip","stop_eclip","name_eclip","score_eclip","strand_eclip","log_fold_enrichment","log_pvalue",
+																				"chrom_vlinc","start_vlinc","stop_vlinc","name_vlinc", "rpkm_vlinc","strand_vlinc","hap1_reads","hap2_reads","binom_pval","skew"])
 ##############
+lines_in_introns = lines.intersect(introns,f=0.5,wa=True)
+binding_in_introns = eclip.intersect(introns,f=0.5,wa=True,s=True,wb=True) # must be stranded
+df_lines_in_introns_coverage = lines_in_introns.coverage(binding_in_introns,F=0.5).to_dataframe(names=["chrom", "start", "stop", "line_name","score",
+																									"strand","eclip_count", "num_bases_covered",
+																									"line_length", "fraction_bound"],
+																							dtype={"chrom":str,"start":int,"stop":int,"line_name":str,
+																							"score":int,"strand":str,"eclip_count":int,
+																							"num_bases_covered":int,"line_length":int,"fraction_bound":float})#############
+# df_lines_in_introns = lines_in_introns.to_dataframe(names = ["chrom", "start", "stop", "line_name", "score", "strand"])
+# # df_lines_in_introns.loc[:, "line_length"] = df_lines_in_introns["stop"] - df_lines_in_introns["start"]
+# binding_in_lines_in_introns = lines_in_introns.intersect()
+
+# print(df_lines_in_introns)
 
 
+for i in range(len(eclip_experiments)):
+	eclip = pybedtools.Bedtool.from_dataframe(df_eclip[df_eclip["name"==eclip_experiments[i]]])
+	observed_binding_rate = df_binding_in_vlincs[df_binding_in_vlincs["name"]==eclip_experiments[i]]
+	get_pvalue_eclip_peaks(length = )
