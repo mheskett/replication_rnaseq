c1de75c22a7f3c9e9a3ad06eaefa3f6da1065582
mheskett <mbheskett@gmail.com>
2019-11-20 17:11:59 -0800

    :d

--- scripts/age.of.lines.py
+++ scripts/age.of.lines.py
@@ -21,7 +21,17 @@ line_age = pd.read_csv("/Users/heskett/replication_rnaseq/annotation.files/warbu
 all_ucsc_lines = pd.read_csv("/Users/heskett/replication_rnaseq/annotation.files/all.l1.elements.txt",header=None,sep="\t",names=["line_name"])
 
 all_lines_df = pd.read_csv("/Users/heskett/replication_rnaseq/annotation.files/ucsc.L1.filtered.hg19.bed",sep="\t",names=["chrom","start","stop","name","score","strand"],
-							header=None)
+							header=None,dtype={"chrom":str,"start":int,"stop":int,"name":str,"score":int,"strand":str})
+
+unique,counts = np.unique(all_lines_df["name"],return_counts=True)
+num_l1s_each_type = dict(zip(unique,counts))
+
+num_l1s_df = pd.DataFrame({"line_name":unique,"number_occurences":counts})
+num_l1s_df.loc[:,"relative_frequency"] = num_l1s_df["number_occurences"] / num_l1s_df["number_occurences"].sum()
+
+# print(num_l1s_df) ## use this to sample from the multiomodal dist
+
+
 asars_file = "/Users/heskett/replication_rnaseq/scripts/gm12878.rep1.hg19Aligned.outgm12878.rep1.expressed.vlincs.skewed.bed"
 lines_file = "/Users/heskett/replication_rnaseq/annotation.files/ucsc.L1.filtered.hg19.bed"
 all_vlincs_file = "/Users/heskett/replication_rnaseq/scripts/gm12878.rep1.hg19Aligned.outgm12878.rep1.expressed.vlincs.all.bed"
@@ -42,30 +52,48 @@ df_all = all_vlincs.intersect(lines,wa=True,wb=True).\
 														dtype={"chrom":str,"start":int,"stop":int,"name_lncrna":str,"rpkm":float,"strand":str,"l1_fraction":float,"hap1_reads":int,"hap2_reads":int,
 														"binom_pval":float,"fdr_pval":float,"fdr_reject":str,"total_reads":int,"skew":float,"chrom_line":str,
 														"start_line":int,"stop_line":int,"name_line":str,"score_line":float,"strand_line":str})
+df_all.to_csv("gm12878.rep1.all.vlincs.intersect.lines.bed",sep="\t",header=None,index=None)
+# df_not_skewed = df_all[(df_all["binom_pval"]>10**-6) | (df_all["skew"]<0.25)]
 
-df_not_skewed = df_all[(df_all["binom_pval"]>10**-6) | (df_all["skew"]<0.25)]
+# age_dict = dict(zip(line_age.line_name, line_age.evolutionary_age_ranking))
 
-age_dict = dict(zip(line_age.line_name, line_age.evolutionary_age_ranking))
 
+# ###
+# all_lines_df.loc[:,"line_age"] = all_lines_df.apply(lambda x: get_age(x["name"], age_dict),axis=1)
+# all_lines_df.to_csv("ucsc.L1.filtered.age.hg19.bed",sep="\t",header=None,index=None)
+# ###
+# df.loc[:,"line_age"] = df.apply(lambda x: get_age(x["name_line"], age_dict),axis=1) # higher number is younger age
+# df_not_skewed.loc[:,"line_age"] = df_not_skewed.apply(lambda x: get_age(x["name_line"], age_dict),axis=1) # higher number is younger age
+
+
+
+# sns.set(font_scale=1.5)
+
+# fig, ax = plt.subplots()
+# sns.kdeplot(df.groupby("name_lncrna")["line_age"].mean(),shade=True,label="LINE_age_score_skewed")
+# # sns.kdeplot(df_not_skewed.groupby("name_lncrna")["line_age"].mean(),shade=True,label="LINE_age_score_biallelic")
+
+# # ax.set_ylim([0,0.19])
+# # ax.set_xlim([-2,30])
+# plt.show()
+# plt.close()
 
 ###
-all_lines_df.loc[:,"line_age"] = all_lines_df.apply(lambda x: get_age(x["name"], age_dict),axis=1)
-print(all_lines_df)
-all_lines_df.to_csv("ucsc.L1.filtered.age.hg19.bed",sep="\t",header=None,index=None)
-###
-df.loc[:,"line_age"] = df.apply(lambda x: get_age(x["name_line"], age_dict),axis=1) # higher number is younger age
-df_not_skewed.loc[:,"line_age"] = df_not_skewed.apply(lambda x: get_age(x["name_line"], age_dict),axis=1) # higher number is younger age
+### first experiment. are there more L1s of any kind (as fraction of total sequence) than 
+### you would expect compared to equivolent length of intergenic sequence.
+### this is like sampling from a jar with two colors of marbles.
 
-sns.set(font_scale=1.5)
+### second experiment. for each specific LINE in each ASAR, does that ASAR contain more 
+### of that specific LINE that you would expect. this is like sampling from a jar where
+## there are as many colors as there are types of LINES. this can be independent
+## of the above experiment.
+## try this numpy.random.multinomial(n, pvals, size=None)
+# n is number of lines in an asar, size is how many times you simulate, pvals is relative freq of lines in human genome
 
-fig, ax = plt.subplots()
-sns.kdeplot(df.groupby("name_lncrna")["line_age"].mean(),shade=True,label="LINE_age_score_skewed")
-# sns.kdeplot(df_not_skewed.groupby("name_lncrna")["line_age"].mean(),shade=True,label="LINE_age_score_biallelic")
+# Use R package EMT:multinomial.test
 
-# ax.set_ylim([0,0.19])
-# ax.set_xlim([-2,30])
-plt.show()
-plt.close()
+
+
 
 # df = df.loc[:,"line_age"] = df.apply(lambda x: age_dict[x["name_line"]],axis=1)
 # print(df)
