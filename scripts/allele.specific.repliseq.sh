9cc56422b144356c8d11b629d15d1040a8c6e0f6
spellmanlab <heskett@ohsu.edu>
2019-12-09 16:42:03 -0800

    d

---
+++ scripts/allele.specific.repliseq.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## start from sorted, index and rmduped BAM file.
+
+input=$1
+b=$(basename $input) ## removes /path/to/file
+filename=${b%.*} ### removes file extension
+out_dir=$2
+
+## get genome coverage
+## this could be used for a simple replication timing early vs late comparison.
+
+bedtools genomecov -bg -ibam $input
+
+## get allele specific counts at heterozygous sites
+## strand agnostic bc this is DNA-seq
+
+bcftools mpileup -R /home/groups/Spellmandata/heskett/replication.rnaseq/platinum.genome/na12878.hg19.het.bed \
+  -f /home/groups/Spellmandata/heskett/myron_refs/human_g1k_v37.fasta \
+  -a DP,AD \ 
+  $1
--- scripts/repliseq.analysis.sh
+++ scripts/repliseq.analysis.sh
@@ -2,20 +2,23 @@
 
 ## start from sorted, index and rmduped BAM file.
 
-input=$1
-b=$(basename $input) ## removes /path/to/file
-filename=${b%.*} ### removes file extension
-out_dir=$2
+early=$1
+late=$2
+e=$(basename $early) ## removes /path/to/file
+early_name=${e%.*} ### removes file extension
+
+l=$(basename $late) ## removes /path/to/file
+late_name=${l%.*} ### removes file extension
+
+out_dir=$3
 
 ## get genome coverage
 ## this could be used for a simple replication timing early vs late comparison.
+## then take the sum across genome windows (non overlapping)
+## then just simply take the log2 ratio of early/late
 
-bedtools genomecov -bg -ibam $input
+bedtools makewindows -w 50000 -s 50000 -g /home/groups/Spellmandata/heskett/myron_refs/human_g1k_v37.fasta.fai > $out_dir'human.g1k.v37.50kb.windows.bed'
+bedtools genomecov -bg -ibam $early | bedtools map -a $out_dir'human.g1k.v37.50kb.windows.bed' -b stdin -c 4 -o sum > $out_dir$early_name.counts.bed 
+bedtools genomecov -bg -ibam $late | bedtools map -a $out_dir'human.g1k.v37.50kb.windows.bed' -b stdin -c 4 -o sum > $out_dir$late_name.counts.bed
 
-## get allele specific counts at heterozygous sites
-## strand agnostic bc this is DNA-seq
-
-bcftools mpileup -R /home/groups/Spellmandata/heskett/replication.rnaseq/platinum.genome/na12878.hg19.het.bed \
-  -f /home/groups/Spellmandata/heskett/myron_refs/human_g1k_v37.fasta \
-  -a DP,AD \ 
-  $1
+# use python for plotting
