57544cb84c43fc60e7d2d2df94b60853dcebfb27
Michael Heskett <michaelheskett@Michaels-Air.attlocal.net>
2025-02-20 11:23:30 -0800

    important change

--- scripts/align.haplotypes.py
+++ scripts/align.haplotypes.py
@@ -28,6 +28,10 @@ if __name__ == "__main__":
 		for line in f:
 			try:
 				tmp = line.rstrip().split()
+				start_pileup = tmp[1]
+				stop_pileup = tmp[2]
+				start_hap = tmp[7]
+				stop_hap = tmp[8]
 				ref = tmp[3]
 				alt = tmp[4].split(",")
 
@@ -45,13 +49,22 @@ if __name__ == "__main__":
 				ref_count = counts[0]
 				alt_count = counts[1]
 
-				if (ref == hap1) and (alt == hap2): ## no switching necessary
+				## first get rid of sites that are within the middle of an indel
+				if start_pileup != start_hap:
+					continue
+				## get rid of indels where the FIRST BASE is the same for hap1 and hap2
+				## the correct thing to do would be to find the first base within the indel
+				## that is not the same between hap1 and hap2, then use that site at the 
+				## mpileup to count hap1 and hap2. too complicated though
+				elif hap1[0] == hap2[0]:
+					continue
+				elif (ref == hap1[0]) and (alt == hap2[0]): ## no switching necessary
 					result += [[tmp[6], tmp[7], tmp[8], hap1, hap2, ref_count, alt_count]]
-				elif (ref == hap2) and (alt == hap1): ## switch
+				elif (ref == hap2[0]) and (alt == hap1[0]): ## switch
 					result += [[tmp[6], tmp[7], tmp[8], hap1, hap2, alt_count, ref_count]]
-				elif ref == hap1 and alt == "<*>":
+				elif ref == hap1[0] and alt == "<*>":
 					result += [[tmp[6], tmp[7], tmp[8], hap1, hap2, ref_count, alt_count]]
-				elif ref == hap2 and alt == "<*>":
+				elif ref == hap2[0] and alt == "<*>":
 					result += [[tmp[6], tmp[7], tmp[8], hap1, hap2, alt_count, ref_count]]
 				else:
 					continue ### these are likely indels that weren't called correctly by mpileup
